5c4cf1efb535c071afd36f94bd7b52a3da487a67
Syed Azeez <syedazeez337@gmail.com>
2024-10-29 23:30:55 +0530

    databases

--- Social/cmd/api/main.go
+++ Social/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Social/cmd/api/internal/db"
 	"Social/cmd/api/internal/env"
 	"Social/cmd/api/internal/store"
 	"log"
@@ -10,13 +11,30 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
-		store: store,
+		store:  store,
 	}
 
 	os.LookupEnv("PATH")
