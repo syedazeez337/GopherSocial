23bb897cba92f99d21f20bfedd8e7fe5cad65914
Syed Azeez <syedazeez337@gmail.com>
2024-10-31 09:30:43 +0530

    Migrations

--- Social/cmd/api/main.go
+++ Social/cmd/api/main.go
@@ -12,7 +12,7 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
